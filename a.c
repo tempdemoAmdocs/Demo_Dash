abe2de8a1199f7b65feabcb622352dbc6de7374e
Debarin Chakraborty <67864244+Debarin123@users.noreply.github.com>
2023-08-11 15:24:16 +0530

    Update a.c

--- a.c
+++ a.c
@@ -2,13 +2,6 @@
 
 int main(int argc, char const *argv[])
 {
-<<<<<<< HEAD
-    printf("o");
-=======
-    printf("Hello");
-    printf("Hello world");
-    
->>>>>>> ab4265d1976f26f98fa4aaf901e9465b65dd6c5c
-    return 0;
+    printf("Nello");
 }
 
